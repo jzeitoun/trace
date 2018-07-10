87fe65bf46bd8a164bdffe064d529f27c54fd9f5
jzeitoun <jzeitoun@uci.edu>
2018-07-09 17:49:21 -0700

    fixed overlay cylinder and corrected axes

--- Pipeline.py
+++ Pipeline.py
@@ -1,30 +1,30 @@
 import tifffile as tif
 import Seed as s
-import Cylinder_Object as cyl
+from Cylinder_Object import Cylinder
 import Optimize
 import Visualization as vis
 
 import Settings
 
 
-def main():
-    Settings.init()
+#def main():
+Settings.init()
 
-    default_radius = 2
-    default_height = 5
+default_radius = 2
+default_height = 5
 
-    # Load in image and seeds
-    filename = 'testdata.tif'
-    img = tif.imread(filename)
-    seedfilename = 'testdata_maxima_binary.tif'
-    seeds = s.get_seeds(seedfilename)
+# Load in image and seeds
+filename = 'testdata.tif'
+img = tif.imread(filename)
+#seedfilename = 'testdata_maxima_binary.tif'
+#seeds = s.get_seeds(seedfilename)
 
-    cylinder = cyl.Cylinder(default_radius, default_height)
-    vis.render_gauss(cylinder)
+cylinder = Cylinder(default_radius, default_height)
+vis.render_gauss(cylinder)
 
-    best_score, best_theta, best_psi = Optimize.optimize_angle(cylinder, seeds[257], img)
+#best_score, best_theta, best_psi = Optimize.optimize_angle(cylinder, seeds[257], img)
 
-    #fit_score = cylinder._score_correlation(img)
+#fit_score = cylinder._score_correlation(img)
 
-if __name__ == '__main__':
-   main()
+#if __name__ == '__main__':
+#   main()
